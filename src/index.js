393827cbda34a5e433a8bfdb36b6a2483f1fc495
Vladimir Stepanov <skretti@gmail.com>
2022-07-16 19:12:38 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let reverseIt = n.toString().split('').reverse().join('');
+	let newReverse;
+	if (reverseIt.endsWith("-")) {
+		let index = reverseIt.indexOf("-");
+		newReverse = "-" + reverseIt.slice(0, index);
+	} else {
+		newReverse = reverseIt;
+	}
+	return newReverse;
 }
